32fc4cbbb4ce8f126d859ca1d0ac459d3141b078
lucaxgomex <lg.projectdev@gmail.com>
2021-08-06 02:41:00 -0300

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,8 @@ const routes = [
     component: Home
   },
   {
-    path: '/creator',
-    name: 'Creator',
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
