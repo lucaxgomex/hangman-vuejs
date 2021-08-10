6983daef8094a436456efb45ed1c2acae857a3f9
lucaxgomex <lg.projectdev@gmail.com>
2021-08-09 23:05:45 -0300

    Setting the display for End component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,23 +5,44 @@
 
     <Word
       :word="word"
-      :hint="hint"/>
+      :hint="hint"
+      :validation="validation"
+      :step="step"/>
+
+    <Keyboard
+      v-if="step === 'game'"
+      :letters="letters"
+      :validation="validation"
+      :play="play"/>
+
+    <End
+      v-if="step !== 'game'"
+      :step="step"
+      :message="step === 'winner' ? 'Congulations' : 'Sorry, you lost'"
+      :playAgain="playAgain"/>
   </div>
 </template>
 
 <script>
 import Stickman from './Stickman'
 import Word from './Word'
+import Keyboard from './Keyboard'
+import End from './End'
 
 export default {
   name: 'Game',
   components: {
-    Stickman, Word
+    Stickman, Word, Keyboard, End
   },
   props: {
     errors: Number,
     word: String,
-    hint: String
+    hint: String,
+    validation: Function,
+    step: String,
+    letters: Array,
+    play: Function,
+    playAgain: Function
   },
   data() {
     return {
