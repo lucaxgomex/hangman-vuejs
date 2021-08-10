44718d709772648dc25fc828e4e20776f9289ef1
lucaxgomex <lg.projectdev@gmail.com>
2021-08-09 23:00:57 -0300

    Component to display a game over message

---
+++ src/components/End.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="end">
+    <div :class="`end-text ${step}`">
+      {{ message }}
+    </div>
+    <button class="end-button" @click="playAgain">
+      Play Again
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'End',
+  props: {
+    step: String,
+    message: String,
+    playAgain: Function
+  }
+}
+</script>
+
+<style>
+.end-text {
+  margin: 20px 0px;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.end-text.hanged {
+  color: var(--color-text-error);
+}
+
+.end-text.winner {
+  color: var(--color-text-success);
+}
+</style>
