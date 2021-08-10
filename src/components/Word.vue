b409a498b6646d58a4e13e3f3c0c47e772fc7ab5
lucaxgomex <lg.projectdev@gmail.com>
2021-08-09 23:03:50 -0300

    Added words validation functions, and Keyboard and End components

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="word">
-    {{ word }}
+    <div class="word-letters">
+      <div class="each-letter" v-for="(letter, key) in word" :key="key">
+        {{ (validation(letter) || step === 'hanged') ? letter : '' }}
+      </div>
+    </div>
+    <div class="display-hint">
+      {{ hint }}
+    </div>
   </div>
 </template>
 
@@ -12,7 +19,9 @@ export default {
   },
   props: {
     word: String,
-    hint: String
+    hint: String,
+    validation: Function,
+    step: String
   },
   data() {
     return {
@@ -26,4 +35,26 @@ export default {
 </script>
 
 <style>
+.word {
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+}
+
+.word-letters {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.each-letter {
+  height: 30px;
+  width: 30px;
+  margin: 0px 5px;
+  border-bottom: 1px solid var(--color-text-light);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
